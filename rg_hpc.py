fc6f4a8602cb9bb3f30b1e612ae297232a72fb77
Will Holdhusen <wholdhus@gmail.com>
2020-05-18 14:23:11 -0400

    Adding the option to increment G or g for less singularities

--- rg_hpc.py
+++ rg_hpc.py
@@ -54,14 +54,10 @@ print('')
 
 dims = (L, Ne, Nw)
 
-if L > Ne + Nw:
-    print('Below half filling!')
-    es, ws, vars_df = solve_rgEqs_1(dims, gf, ks, dg=dg, g0=g0, imscale_k=imk,
-                                    imscale_v=imv)
-else:
-    print('Above half filling!')
-    vars_df = solve_rgEqs_2(dims, gf, ks, dg=dg, g0=g0, imscale_k=imk,
-                                    imscale_v=imv, skip=4)
+increment_G = L < 10
+
+vars_df = solve_rgEqs_2(dims, gf, ks, dg=dg, g0=g0, imscale_k=imk,
+                        imscale_v=imv, skip=4, increment_G=increment_G)
 
 print('Done! Putting things in a CSV')
 vars_df.to_csv(RESULT_FP + 'solutions_full_{}_{}_{}.csv'.format(L, N, gf))
